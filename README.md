ad719c8c316c8e980d4ddb449643ddfe1d53ced6
NadhifAdyatmaZ <nadhifadyat@gmail.com>
2023-03-07 21:40:05 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# signin_example
+
+Tugas 4 Flutter (Signin Example)
+
+![Page login](images/form.png)
+![Page Home](images/dashboard.png)
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+import 'myPage/home.dart';
+import 'myPage/signupscreen.dart';
+
+void main() => runApp(const SignUpApp());
+
+class SignUpApp extends StatelessWidget {
+  const SignUpApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      routes: {
+        '/': (context) => const SignUpScreen(),
+        '/welcome': (context) => const WelcomeScreen(),
+      },
+    );
+  }
+}
